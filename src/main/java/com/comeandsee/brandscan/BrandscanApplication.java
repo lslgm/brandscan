d596806af3053d4e8ce1e65dd782ad7ba781e75e
DESKTOP-3T7OJU9\woori <khkevin2@gmail.com>
2023-11-13 15:38:49 +0900

    프로젝트 생성 커밋

---
+++ src/main/java/com/comeandsee/brandscan/BrandscanApplication.java
@@ -0,0 +1,13 @@
+package com.comeandsee.brandscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandscanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrandscanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/comeandsee/brandscan/BrandscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comeandsee.brandscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandscanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
