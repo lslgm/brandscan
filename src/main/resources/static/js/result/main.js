03ba0179458f1ac83dfbfbd655d1a287ba16dc60
woori <wpdnr96@naver.com>
2023-11-23 10:42:29 +0900

    결과페이지/상세페이지 완료

---
+++ src/main/resources/static/js/result/main.js
@@ -0,0 +1,24 @@
+/*
+	Miniport by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$nav = $('#nav');
+
+	// Breakpoints.
+
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+
+})(jQuery);
